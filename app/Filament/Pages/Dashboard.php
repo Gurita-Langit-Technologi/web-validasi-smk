da53b98c8d7a37bfd053cef265dd0da41c125651
achriyatuls <achriyatuls0101@gmail.com>
2025-08-18 16:20:45 +0700

    blank

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,8 +2,7 @@
 
 namespace App\Filament\Pages;
 
-//use Filament\Actions\Action;
-//use Filament\Forms\Components\FileUpload;
+
 
 class Dashboard extends \Filament\Pages\Dashboard
 {
@@ -11,28 +10,8 @@ class Dashboard extends \Filament\Pages\Dashboard
     public function getColumns(): int | string | array
     {
         return [
-            'md' => 4,
+            'md' => 6,
             'xl' => 5,
         ];
     }
-
-    /*   protected function getHeaderActions(): array
-    {
-        return [
-            Action::make('Import Data')
-                ->icon('heroicon-o-arrow-up-tray')
-                ->form([
-                    FileUpload::make('file')
-                        ->label('Pilih file untuk diimpor')
-                        ->acceptedFileTypes(['.csv', '.xlsx'])
-                        ->required(),
-                ])
-                ->action(function (array $data) {
-                    $this->importFile($data['file']);
-                })
-                ->modalHeading('Impor Data')
-                ->modalSubmitActionLabel('Impor'),
-        ];
-    }
-        */
 }
